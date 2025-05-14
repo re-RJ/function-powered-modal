91bae9e3fb666f8684e0ab9aa3aed37f1496185d
rj_m <rjmarte@sbcglobal.net>
2025-05-14 16:53:25 -0700

    Modified file name from basic EventListener

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ var button = document.querySelector(".got-this");
 var modal = document.querySelector(".modal");
 var modalX = document.querySelector(".modal-x");
 
-button.addEventListener("click", function() {
-    modal.classList.add("show-modal");
-    button.innerText = "You've got this!!!";
+button.addEventListener("click", function () {
+  modal.classList.add("show-modal");
+  button.innerText = "You've got this!!!";
 });
 
-modalX.addEventListener("click", function() {
-    modal.classList.remove("show-modal");
-    button.innerText = "Who's got this?";
-})
+modalX.addEventListener("click", function () {
+  modal.classList.remove("show-modal");
+  button.innerText = "Who's got this?";
+});
