fa0691af10a764f3d1936b715a68f862c0e42cad
rj_m <rjmarte@sbcglobal.net>
2025-05-14 17:17:08 -0700

    Function Powered modal changes: Enter/Escape button open/closer

--- js/script.js
+++ js/script.js
@@ -1,13 +1,53 @@
+//// Prior Exercise: BUTTON AND EXIT-X ////
+
+//var button = document.querySelector(".got-this");
+//var modal = document.querySelector(".modal");
+//var modalX = document.querySelector(".modal-x");
+
+//button.addEventListener("click", function () {
+//modal.classList.add("show-modal");
+//button.innerText = "You've got this!!!";
+//});
+
+//modalX.addEventListener("click", function () {
+//modal.classList.remove("show-modal");
+//button.innerText = "Who's got this?";
+//});
+
+//// FUNCTION POWERED MODAL ////
+
 var button = document.querySelector(".got-this");
 var modal = document.querySelector(".modal");
 var modalX = document.querySelector(".modal-x");
 
-button.addEventListener("click", function () {
-  modal.classList.add("show-modal");
-  button.innerText = "You've got this!!!";
-});
-
-modalX.addEventListener("click", function () {
+var closeModal = function () {
   modal.classList.remove("show-modal");
   button.innerText = "Who's got this?";
+};
+
+var openModal = function () {
+  modal.classList.add("show-modal");
+  button.innerText = "You've got this!!!";
+};
+
+button.addEventListener("click", function () {
+  openModal();
+});
+
+
+modalX.addEventListener("click", function () {
+  closeModal();
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    if (modal.classList.contains("show-modal")) {
+      closeModal();
+    }
+    //else if not part of exercise, messed around and figure out "on/off" buttons    
+  } else if (e.key === "Enter") {
+    if (modal.classList !== "show-modal") { //works with [modal.classList.not(contains("")) for all but first time]
+      openModal();
+    }
+  }
 });
